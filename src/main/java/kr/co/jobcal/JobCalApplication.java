f9db927bff73556eecbe0c084694f07480918d2a
이정민 <kyungheeyj45@ajou.ac.kr>
2026-02-11 02:49:12 +0900

    init: JobCal 프로젝트 세팅

---
+++ src/main/java/kr/co/jobcal/JobCalApplication.java
@@ -0,0 +1,13 @@
+package kr.co.jobcal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobCalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobCalApplication.class, args);
+    }
+
+}
